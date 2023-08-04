5cb706eac411c4898765657bef20c9450a95d08b
francisco.villalba <francisco.villalba@calimatechnologies.com>
2023-08-04 17:21:48 -0400

    mejoras

--- ticket/views.py
+++ ticket/views.py
@@ -1,3 +1,5 @@
+import os
+from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
@@ -5,25 +7,18 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, ListView, UpdateView
-
+from django.views.generic import CreateView, ListView, UpdateView 
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
-from reportlab.lib.pagesizes import letter
+from reportlab.lib.pagesizes import letter, landscape
 from reportlab.pdfgen import canvas
 from .models import Ticket_Cab
-
-from ticket.forms import ProveedorForm, TicketHeaderForm, TicketDetailFormSet, TicketDetailForm
-from ticket.models import Ticket_Cab, Proveedores, Ticket_det
-
-from reportlab.platypus import Table, TableStyle
-
-import socket
-import threading
-import serial
-
-import websocket
-from django.http import JsonResponse
+from ticket.forms import ProveedorForm, TicketHeaderForm, TicketDetailFormSet
+from ticket.models import Ticket_Cab, Proveedores, Ticket_det 
+from reportlab.platypus import Table, TableStyle 
+import socket 
+from django.http import JsonResponse  
+from django.db.models import Sum
 
 
 # Create your views here.
@@ -138,49 +133,8 @@ class TicketUpdateView(UpdateView):
             print("detail_formset errors:", [form.errors for form in detail_formset])
 
         return redirect('panel-principal')
-# class TicketUpdateView(UpdateView):
-#     model = Ticket_Cab
-#     fields = ['proveedor', 'tipo_de_recoleccion', 'estado', 'created_by']
-#
-#     def get(self, request, *args, **kwargs):
-#         header_form = TicketHeaderForm()
-#         detail_formset = TicketDetailFormSet()
-#
-#         return render(request, 'modificar_ticket.html', {'header_form': header_form, 'detail_formset': detail_formset})
-#
-#     def post(self, request):
-#         self.object = self.get_object()
-#         header_form = TicketHeaderForm(request.POST, instance=self.object)
-#         detail_formset = TicketDetailFormSet(request.POST, instance=self.object)
-#
-#         if header_form.is_valid() and detail_formset.is_valid():
-#             header = header_form.save(commit=False)
-#             header.created_by = request.user
-#             header.save()
-#             details = detail_formset.save(commit=False)
-#
-#             for detail in details:
-#                 detail.created_by = request.user
-#                 detail.ticketCabId = header
-#                 detail.save()
-#             else:
-#                 print("header_form errors:", header_form.errors)
-#                 print("detail_formset errors:", [form.errors for form in detail_formset])
-#             return redirect('panel-principal')
-#
-#         return redirect('panel-principal')
-
-
-
-
-
-
-
-
-
-
-
-
+ 
+ 
 @method_decorator(login_required, name='dispatch')
 class panel_proveedores(ListView):
     model = Proveedores
@@ -216,46 +170,17 @@ class modificar_proveedor(UpdateView):
 
     success_url = reverse_lazy('panel-proveedores')
 
-
-
-
-
-# def generar_pdf_and_update_estado(request, id):
-#     # Fetch the specific Ticket_Cab record from the model using get_object_or_404
-#     ticket_cab = get_object_or_404(Ticket_det, ticketCabId=id)
-#     ticket_det = get_object_or_404(Ticket_Cab, pk=id)
-
-#     # Change the estado field to False
-#     ticket_det.estado = False
-#     ticket_det.save()
-
-#     # Create a response object with PDF content type
-#     response = HttpResponse(content_type='application/pdf')
-
-#     # Set the filename for the PDF
-#     response['Content-Disposition'] = f'attachment; filename="data_report_{id}.pdf"'
-
-#     # Create a PDF canvas
-#     pdf = canvas.Canvas(response, pagesize=letter)
-
-#     # Add content to the PDF
-#     pdf.drawString(100, 700, f"Proveedor: {ticket_cab.peso1}")
-#     pdf.drawString(100, 680, f"Tipo de recoleccion: {ticket_cab.peso2}")
-    
-    
-
-#     # Save the PDF content
-#     pdf.save()
-
-#     #return redirect('panel-principal')
-#     return response
-
-
+ 
 
 def generar_pdf_actualizar_estado(request, id):
     # Fetch the specific Ticket_Cab record from the model using get_object_or_404
-    ticket_cab = get_object_or_404(Ticket_Cab, pk=id)
+    ticket_cab = get_object_or_404(Ticket_Cab, pk=id) 
     ticket_det_list = Ticket_det.objects.filter(ticketCabId=id)
+
+    # total_peso1 = sum(ticket_det_list.peso1.all())
+    total_peso1 = ticket_det_list.aggregate(total_peso1=Sum('peso1'))['total_peso1']
+    total_peso2 = ticket_det_list.aggregate(total_peso2=Sum('peso2'))['total_peso2']
+    total_peso_bruto = ticket_det_list.aggregate(total_peso_bruto=Sum('peso_bruto'))['total_peso_bruto']
 
     # Cambio el estado a False del Ticket_Cab
     ticket_cab.estado = False
@@ -268,19 +193,39 @@ def generar_pdf_actualizar_estado(request, id):
     response['Content-Disposition'] = f'attachment; filename="ticket_numero_{id}.pdf"'
 
     # Creamos la estructura del PDF
-    pdf = canvas.Canvas(response, pagesize=letter)
+    pdf = canvas.Canvas(response, pagesize=landscape(letter))
+ 
+    left_margin, bottom_margin, right_margin, top_margin = 50, 50, 50, 50
 
+     # Agrega una imagen al PDF 
+    imagen_path = os.path.join(settings.BASE_DIR, 'static', 'img', 'yr.png')
+    pdf.drawImage(imagen_path, 30, 550, width=180, height=50)
+
+     
     # Agregamos contenido al PDF
-    pdf.drawString(100, 700, f"Proveedor: {ticket_cab.proveedor}")
-    pdf.drawString(100, 680, f"Tipo de recoleccion: {ticket_cab.tipo_de_recoleccion}")
+    pdf.drawString(left_margin + 25, letter[0] - top_margin - 30, f"Procesado por: {ticket_cab.created_by}")
+    pdf.drawString(left_margin + 25, letter[0] - top_margin - 50, f"Impreso por: { request.user }")
+    pdf.drawString(left_margin + 400, letter[0] - top_margin - -10, f"Nro. Ticket: {ticket_cab.nro_ticket}")
+    pdf.drawString(left_margin + 400, letter[0] - top_margin - 10, f"Empresa: {ticket_cab.proveedor}")
+    pdf.drawString(left_margin + 400, letter[0] - top_margin - 30, f"Matricula: {ticket_cab.matricula}")
+    pdf.drawString(left_margin + 400, letter[0] - top_margin - 50, f"Chofer: {ticket_cab.chofer}")
 
     
+    
     # Creamos una tabla con los headers y las filas de datos
-    table_data = [['Producto', 'Peso1', 'Peso2']]
+    table_data = [['Producto', 'Peso1', 'Peso2', 'Peso Bruto']]
     for ticket_det in ticket_det_list:
-        table_data.append([ticket_det.producto, ticket_det.peso1, ticket_det.peso2])
+        table_data.append([ticket_det.producto, ticket_det.peso1, ticket_det.peso2, ticket_det.peso_bruto])
 
-    table = Table(table_data)
+    table_data.append(["Total", total_peso1, total_peso2, total_peso_bruto])
+
+     # Calculate column widths
+    num_cols = len(table_data[0])
+    table_width = 680  # Total width of the page in points (letter size is 612x792 points)
+    col_width = table_width / num_cols
+    col_widths = [col_width] * num_cols
+    table = Table(table_data, colWidths=col_widths)
+    # table = Table(table_data)
 
     # Damos estilo a la tabla
     style = TableStyle([
@@ -291,63 +236,27 @@ def generar_pdf_actualizar_estado(request, id):
         ('BOTTOMPADDING', (0, 0), (-1, 0), 12),    # Header padding
         ('BACKGROUND', (0, 1), (-1, -1), 'white'),  # Data background color
         ('GRID', (0, 0), (-1, -1), 1, 'black'),    # Table grid color
+        
     ])
     table.setStyle(style)
 
     # Dibujamos la tabla en el PDF
-    table.wrapOn(pdf, 500, 100)
-    table.drawOn(pdf, 100, 600)
+    table.wrapOn(pdf, 400, 50)
+    table.drawOn(pdf, 50, 400)
 
     # Guardamos el pdf
     pdf.save()
 
     return response
 
-
-
-# Conexión a la báscula
-
-# def connect_to_websocket(request):
-#     websocket_server_url = '10.20.1.125:8059'  # Replace this with the WebSocket server URL
-#     print('estoy aqui')
-#     try:
-#         print('hola')
-#         # Connect to the WebSocket server
-#         ws = websocket.create_connection(websocket_server_url)
-#         print(ws)
-        
-#         # Send data to the WebSocket server (optional)
-#         data_to_send = 'Leer bascula'
-#         sendmsg = data_to_send.encode('ascii')
-#         hola = ws.send(sendmsg)
-        
-#         print(hola)
-
-#         # Receive data from the WebSocket server (optional)
-#         response_data = ws.recv()
-
-#         # Close the WebSocket connection
-#         ws.close()
-
-#         return JsonResponse({'response': response_data})
-#     except websocket.WebSocketException as e:
-#         return JsonResponse({'error': str(e)})
-
-
-import socket
-
+ 
 def LeerPeso(request):
-    print('hola')
-    """Main function."""
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print(client)
     client.connect(("10.20.1.125", 8059))
     message = "Leer bascula"
-    
-    #convert = message.encode('ascii')
-    #send_message(client, convert)
-    client.sendall(message.encode('ascii'))
-    #message = receive_message(client)
+     
+    client.sendall(message.encode('ascii')) 
     message = client.recv(1024).decode()
     if message == '0':
         message = '0'    
@@ -356,32 +265,6 @@ def LeerPeso(request):
     client.close()
 
     return JsonResponse({'message': message})
-    # context = {'message': message}
-    # return render(request, 'modificar_ticket.html', context)
-    
-    # return HttpResponse(message)
-
-
-# def hola(request):
-#     # Connect to the server
-#     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     client.connect(("10.20.1.125", 8059))
-
-#     # Send a message to the server
-#     message = "Leer bascula"
-#     client.sendall(message.encode('ascii'))
-
-#     # Receive the response from the server
-#     response = client.recv(1024).decode()
-#     client.close()
-
-#     # Process the response and return it as an HttpResponse
-#     if response == '0':
-#         response_message = 'Received 0 from server'
-#     else:
-#         response_message = f'Received: {response} from server'
-
-#     return HttpResponse(response_message)
  
 
 
